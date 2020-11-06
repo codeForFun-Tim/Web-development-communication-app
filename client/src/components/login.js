4529011f84ecec4371bbeb5ada9a8076323d6671
andyzhugz <guangzhe@sas.upenn.edu>
2020-11-05 22:01:00 -0500

    Login minor update

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -1,16 +1,17 @@
 import React, { useState } from 'react'
 import '../stylesheets/LoginRegister.css'
-const Login = () => {
 
+const Login = () => {
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
 
   const handleLogin = () => {
-    setError(null);
+    setError(false);
     setLoading(true);
     fetch('http://localhost:8080/users/login', { username: username.value, password: password.value }).catch(error => {
+      // wait for backend
       // if (error.response.status === 401) setError(error.response.data.message);
       // else 
       setLoading(false);
@@ -26,8 +27,7 @@ const Login = () => {
             <form className="formLogin">
                 <input className="userName" type="text" align="center" placeholder="Username" />
                 <input className="pass" type="password" align="center" placeholder="Password" />
-                <p className="forgotPwd" align="center"><a href="#" />Forgot Password?</p>
-                {error && <><small style={{ color: 'red' }}>{error}</small><br /></>}<br />
+                <p className="forgotPwd" align="center"><a href="#" />Forgot Password? {error ? <><br /><small>{'Failed to login'}</small></> : <><br /><small style={{ color: "white"}}>{'.'}</small></>}</p>
                 <a className="loginBtn" align="center" onClick={handleLogin}><b>{loading ? 'Loading' : 'Sign in  '}</b></a>
                 <a className="registerBtn" align="center"><b>Register</b></a>
             </form>  
