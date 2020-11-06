430de5209279e7bc2ce68c84638c7f989541c751
andyzhugz <guangzhe@sas.upenn.edu>
2020-11-06 10:54:04 -0500

    Add error

--- client/src/components/login.js
+++ client/src/components/login.js
@@ -3,12 +3,11 @@ import { Link } from 'react-router-dom';
 import '../stylesheets/LoginRegister.css'
 
 const Login = () => {
-
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [error, setError] = useState(false);
   const [loading, setLoading] = useState(false);
-
+  
   const handleLogin = () => {
     setError(false);
     setLoading(true);
@@ -20,24 +19,28 @@ const Login = () => {
       setError("Something went wrong. Please try again later.");
     });
   }
-  
+
   return  (
     <div>
         <h1 align="center">Welcome to Our App</h1>
         <div className="main">
             <p className="sign" align="center">Login</p>
-            <form class="formLogin">
+            <form className="formLogin">
                 <input className="userName" type="text" align="center" placeholder="Username: cat@gmail.com" />
                 <input className="pass" type="password" align="center" placeholder="Password" />
                 <p className="forgotPwd" align="center"><a href="#" />Forgot Password?</p>
-                <button className="loginBtn" align="center"><b>Login</b></button>
+                <input type="button" value={loading ? 'Loading' : 'Sign In'} onClick={handleLogin} disabled={loading} className="loginBtn" />
                 <Link to='/Register'>
-                    <button className="registerBtn" align="center">
-                        <b>Register</b>
-                    </button>
+                  <button className="registerBtn" align="center">
+                    <b>Register</b>
+                  </button>
                 </Link>
+                {error && <><div style={{ color: 'red', fontSize: 12, textAlign: "center" }}>{error}</div><br /></>}<br />
+
             </form>  
         </div>
+        <>         
+        </>
     </div>
     );
 }
