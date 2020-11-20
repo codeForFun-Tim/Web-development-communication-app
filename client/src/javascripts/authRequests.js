191e51466fa09a742d4d3f2683725c64cfeed2d1
codeForFun-Tim <timqi@seas.upenn.edu>
2020-11-20 14:34:04 -0500

    client/src/javascripts
    Connection code for the Frontend and Backend

---
+++ client/src/javascripts/authRequests.js
@@ -0,0 +1,77 @@
+/* globals fetch FormData */
+
+import { api } from '../api';
+import axios from 'axios';
+
+async function register(email, username, password) {
+  
+try{
+    console.log("api url Register: ",`${api.url}/Register`);
+  await axios.post(`${api.url}/Register`,
+ 
+  `email=${email}&username=${username}&password=${password}`
+  );
+}
+catch(err){
+    console.log(err);
+}
+
+}
+
+async function login(email, password) {
+    try{
+        await axios.post(`${api.url}/login`,
+        `email=${email}&password=${password}`
+        );
+    }
+    catch(err){
+        console.log(err);
+    }
+//   return fetch(`${api.url}/login`,
+//     {
+//       method: 'POST',
+//       body: JSON.stringify({
+//         email,
+//         password,
+//       }),
+//       headers: {
+//         'Content-type': 'application/json; charset=UTF-8',
+//         Accept: 'application/json; charset=UTF-8',
+//       },
+//       credentials: 'include',
+//       mode: 'cors',
+//     });
+}
+
+async function logout() {
+  return fetch(`${api.url}/logout`,
+    {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        Accept: 'application/json; charset=UTF-8',
+      },
+      credentials: 'include',
+      mode: 'cors',
+    });
+}
+
+async function checkAuth() {
+  return fetch(`${api.url}/checkAuth`,
+    {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json; charset=UTF-8',
+        Accept: 'application/json; charset=UTF-8',
+      },
+      credentials: 'include',
+      mode: 'cors',
+    });
+}
+
+export {
+  register,
+  login,
+  logout,
+  checkAuth,
+};
