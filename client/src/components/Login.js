9f4771da61aea68288e897270211ffaff6e4e75c
andyzhugz <guangzhe@sas.upenn.edu>
2020-11-20 15:50:39 -0500

    Create Login.js

---
+++ client/src/components/Login.js
@@ -0,0 +1,48 @@
+import React, { useState, useEffect } from 'react'
+import { Link } from 'react-router-dom';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { login } from '../javascripts/authRequests'
+import '../stylesheets/LoginRegister.css'
+
+const Login = () => {
+  //const [username, setUsername] = useState("");
+  //const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+  
+  const handleLogin = () => {
+    const name = document.getElementById('userName').value;
+    const pwd = document.getElementById('password').value;
+    setError(false);
+    setLoading(true);
+    // leave axios code here
+    //
+    login(name, pwd);
+    setLoading(false);
+  }
+
+  return  (
+    <div>
+        <h1 align="center">Welcome to Our App</h1>
+        <div className="main">
+            <p className="sign" align="center">Login</p>
+            <form className="formLogin">
+                <input className="userName" id="userName" type="text" align="center" placeholder="Username: cat@gmail.com" />
+                <input className="pass" id="password" type="password" align="center" placeholder="Password" />
+                <p className="forgotPwd" align="center"><a href="#" />Forgot Password?</p>
+                <input type="button" id="loginBtn" value={loading ? 'Loading' : 'Sign In'} onClick={handleLogin} disabled={loading} className="loginBtn" />
+                <Link to='/Register'>
+                  <button id="registerBtn" className="registerBtn" align="center">
+                    <b>Register</b>
+                  </button>
+                </Link>
+                {error && <><div id="error" style={{ color: 'red', fontSize: 12, textAlign: "center" }}>{error}</div><br /></>}<br />
+            </form>  
+        </div>
+        <>         
+        </>
+    </div>
+    );
+}
+
+export default Login;
