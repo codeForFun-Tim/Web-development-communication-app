ec7047fd305bfc7e1cc8b7c6285a069fab451049
andyzhugz <guangzhe@sas.upenn.edu>
2020-12-21 15:59:48 -0500

    test

---
+++ client/src/unitTest/profile.test.js
@@ -0,0 +1,55 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { act } from 'react-dom/test-utils';
+import {Profile, validation} from '../components/Profile.js';
+
+const mockHistoryPush = jest.fn();
+
+jest.mock('react-router-dom', () => ({
+  ...jest.requireActual('react-router-dom'),
+  useHistory: () => ({
+    push: mockHistoryPush,
+  }),
+}));
+
+const renderer = require('react-test-renderer');
+
+describe('Independent function tests', () => {
+    test('validation function true', () => {
+        expect(validation('123456789')).toBe(true);
+    });
+
+    test('validation function false', () => {
+        expect(validation('1')).toBe(false);
+    });
+});
+
+// snapshot testings
+describe('Test snapshot', () => {
+  test('snapshot', () => {
+    const component = renderer.create(<Profile />);
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
+
+// functonal component functions tests
+describe('Test functonal component functions', () => {
+  let container;
+  beforeEach(() => {
+    container = document.createElement('div');
+    document.body.appendChild(container);
+  });
+
+  afterEach(() => {
+    document.body.removeChild(container);
+    container = null;
+  });
+
+  test('Test function inside profile component', () => {
+    act(() => {
+      ReactDOM.render(<Profile />, container);
+    });
+  });
+
+});
