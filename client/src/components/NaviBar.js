32582b95f2f18b63eb41b8bc62f71ec50c7ba390
andyzhugz <guangzhe@sas.upenn.edu>
2020-12-20 21:07:33 -0500

    Update NaviBar.js

--- client/src/components/NaviBar.js
+++ client/src/components/NaviBar.js
@@ -33,7 +33,7 @@ const NaviBar = () => {
           <a href="/status">Status</a>
         </li>
         <li className="nav_li">
-          <a href="/#" onClick={logOut}>Logout</a>
+          <a href="/login" onClick={logOut}>Logout</a>
         </li>
         <li className="nav_li">
           <a href="/main">{username}</a>
